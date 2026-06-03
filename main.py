87863a0b7da908e74022491594fd89eda39226f9
misha <jjhjhj@gmail.com>
2026-06-03 23:54:12 +0300

    Second_Commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import cv2;
-import time;
-import numpy as np;
+import cv2
+import time
+import numpy as np
 from ultralytics import YOLO
+from sklearn.cluster import KMeans
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolo11s.pt")
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("test.mp4")
 count_frame = 0
 FPS = 0
 
@@ -26,9 +27,19 @@ if cap.isOpened() == False:
 ret, frame = cap.read()
 Resolution = np.shape(frame)
 
+def InitBGR(crop):
+    h, w = crop.shape[:2]
+
+    t_shirt_region = crop[int(h * 0.3):int(h * 0.5),int(w * 0.35):int(w * 0.65)]
+    B_mean = np.mean(t_shirt_region[:, :, 0])
+    G_mean = np.mean(t_shirt_region[:, :, 1])
+    R_mean = np.mean(t_shirt_region[:, :, 2])
+
+    return B_mean,G_mean,R_mean
+
 
 def StartTracking(frame):
-    results = model.track(frame, persist=True, tracker="bytetrack.yaml", verbose=False)
+    results = model.track(frame, persist=True, tracker="bytetrack.yaml",classes=[0],conf=0.2,verbose=False)
     boxes = results[0].boxes
 
     for box in boxes:
@@ -38,11 +49,31 @@ def StartTracking(frame):
         cls = int(box.cls[0].item())
         ID = int(box.id[0].item())
         if cls == 0:
-            cv2.putText(frame, f"ID: {ID}  {results[0].names[cls]} {conf:.2f}", (x1, y1 + 25), cv2.FONT_HERSHEY_SIMPLEX,
-                        1, (255, 0, 0), thickness=2)
-            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f"ID: {ID}  {conf:.2f}", (x1-25, y1 - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6, (0, 255, 255), thickness=2)
+
+            player_crop = frame[y1:y2, x1:x2]
+
+            B,G,R = InitBGR(player_crop)
+
+
+            foot_x = (x1 + x2) // 2
+            foot_y = y2
+            scale = x2-x1
+
+            cv2.ellipse(
+                frame,
+                (foot_x, foot_y),
+                (scale, 12),
+                0,
+                320,
+                590,
+                (0, 255, 255),
+                3
+            )
 
     return frame
+
 
 
 def draw_overlay(frame):
@@ -66,7 +97,6 @@ def KeyFunctions(pause, Canny, grayscale,tracking,countinueValue):
 
     elif key == ord('t'):
         tracking = not tracking
-
 
     elif key == ord('g'):
         grayscale = not grayscale
@@ -98,11 +128,13 @@ while countinueValue:
 
         count_frame += 1
         current_time = time.time()
+
         if count_frame % 50 == 0:
             FPS = 1 / float(current_time - prev_time)
 
         prev_time = current_time
         frame = draw_overlay(frame)
+    frame = cv2.resize(frame, (980, 540))
     cv2.imshow('frame',frame)
 
     pause, Canny, grayscale,tracking, countinueValue = KeyFunctions(pause, Canny, grayscale,tracking,countinueValue)
